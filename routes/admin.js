29e16213b3ecacab780356a8d4ee5165bca14022
Roby Awaludin Fajar <222011392@stis.ac.id>
2022-12-05 09:21:05 +0700

    product crud almost done

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const adminRouter = express.Router();
 const admin = require('../middlewares/admin');
 const Product = require('../models/product');
 
-// ADD PRODUCT
+// ADD BOOK PRODUCT
 adminRouter.post('/admin/add-product', admin, async(req, res) => {
     try {
         const {productName, authorName, publisher, yearPublished, description, price, quantity, genre, category, images} = req.body;
@@ -20,6 +20,28 @@ adminRouter.post('/admin/add-product', admin, async(req, res) => {
             images, 
         });
         product = await product.save();
+        res.json(product);
+    } catch (e) {
+        res.status(500).json({error: e.message});
+    }
+});
+
+// GET ALL BOOKS PRODUCT
+adminRouter.get('/admin/get-all-products', admin, async(req, res) => {
+    try {
+        const product = await Product.find({});
+        res.json(product);
+    } catch (e) {
+        res.status(500).json({error: e.message});
+    }
+});
+
+// DELETE BOOKS PRODUCT
+adminRouter.post('/admin/delete-product', admin, async (req, res) => {
+    try {
+        const {id} = req.body;
+        let product = await Product.findByIdAndDelete(id);
+        // product = await product.save();
         res.json(product);
     } catch (e) {
         res.status(500).json({error: e.message});
